82d9225ffc90d6364ea0e7217f707544c0a948a0
ramroumette <mr.toumirami@outlook.fr>
2024-03-09 19:07:23 +0100

    implements the main : collect all the component in one function

--- src/main/java/org/ratomidev/Main.java
+++ src/main/java/org/ratomidev/Main.java
@@ -1,7 +1,13 @@
 package org.ratomidev;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)  throws IOException{
+        BankStatementParser bankStatementParser = new BankStatementCSVParser();
+        BankStatementAnalyser bankStatementAnalyser = new BankStatementAnalyser();
+        bankStatementAnalyser.analyse("transactions.csv",
+                bankStatementParser);
+        System.out.println("work");
+
     }
 }
