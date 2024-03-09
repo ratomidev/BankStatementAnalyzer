5515c992f4927888b03aef9e32829fc10d05d49c
ramroumette <mr.toumirami@outlook.fr>
2024-03-09 18:45:20 +0100

    make an interface BankStatementProcessor to decouple code and update BankStatementCSVParser

---
+++ src/main/java/org/ratomidev/BankStatementParser.java
@@ -0,0 +1,10 @@
+package org.ratomidev;
+
+import org.ratomidev.model.BankTransaction;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
