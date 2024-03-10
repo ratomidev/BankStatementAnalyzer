06686df50bf74fa4f56b289a52313c153a049b6d
ramroumette <mr.toumirami@outlook.fr>
2024-03-10 17:19:48 +0100

    deleted:    s         deleted:    src/main/java/org/ratomidev/BankTransactionsFilter.java

--- src/main/java/org/ratomidev/BankTransactionsFilter.java
+++
@@ -1,9 +0,0 @@
-package org.ratomidev;
-
-import org.ratomidev.model.BankTransaction;
-
-@FunctionalInterface
-public interface BankTransactionsFilter {
-    public boolean test(BankTransaction bankTransaction);
-
-}
