ac7758983ddc647e6e0d0824316204be251c4a90
ramroumette <mr.toumirami@outlook.fr>
2024-03-10 17:26:11 +0100

    change the return type of the analyse methode to be SummaryStatics

--- src/main/java/org/ratomidev/BankStatementAnalyser.java
+++ src/main/java/org/ratomidev/BankStatementAnalyser.java
@@ -1,6 +1,7 @@
 package org.ratomidev;
 
 import org.ratomidev.model.BankTransaction;
+import org.ratomidev.model.SummaryStatistics;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,20 +13,26 @@ import java.util.List;
 public class BankStatementAnalyser {
     private static final String RESOURCES = "src/main/resources/";
 
-    public void analyse(String filename,
+    public SummaryStatistics analyse(String filename,
                                BankStatementParser bankStatementCSVParser
                                ) throws IOException {
         final Path path = Paths.get(RESOURCES+filename);
         final List<String> lines = Files.readAllLines(path);
+
         final List<BankTransaction> bankTransactions =
                 bankStatementCSVParser.parseLinesFrom(lines);
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-        collectSummary(bankStatementProcessor);
+
+        final SummaryStatistics summaryStatistics = new SummaryStatistics(bankStatementProcessor.calculateTotalAmount(),
+                bankStatementProcessor.calculateMaxAmount(),
+                bankStatementProcessor.calculateMinAmount(),
+                bankStatementProcessor.calcultateAverageAmount());
+        return summaryStatistics;
 
     }
     private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
         System.out.println("total of all months " + bankStatementProcessor.calculateTotalAmount());
-        System.out.println("total of Junary " + bankStatementProcessor.calculateTotalAmount(Month.JANUARY));
+        System.out.println("total of Junary " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
         System.out.println("transaction that grater than 1000");
         bankStatementProcessor.findTransactionsGreaterThanEqual(6000)
                 .stream().forEach(System.out::println);
@@ -34,11 +41,10 @@ public class BankStatementAnalyser {
         bankStatementProcessor.findTransactionsInMonth(Month.JANUARY)
                 .stream().forEach(System.out::println);
 
-        System.out.println("Transaction that in month with amount");
-        bankStatementProcessor.findTransactionsInMonthAndGraterAmount(Month.JANUARY, -75)
-                .forEach(System.out::println);
         System.out.println("the last feature");
         bankStatementProcessor.findTransactions(bankTransaction ->
                 bankTransaction.getAmount()==6000 || bankTransaction.getDescription().equals("Cinema") ).forEach(System.out::println);
+        System.out.println("calculate the average amount");
+        System.out.println(bankStatementProcessor.calcultateAverageAmount());
     }
 }
