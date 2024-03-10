44e5968986b04b88f2d7603f8aabfd827a83377c
ramroumette <mr.toumirami@outlook.fr>
2024-03-10 17:32:16 +0100

    cover some scenario in testing

--- src/test/java/BankStatementCSVParserTest.java
+++ src/test/java/BankStatementCSVParserTest.java
@@ -12,7 +12,7 @@ public class BankStatementCSVParserTest {
             = new BankStatementCSVParser();
 
     @Test
-    public void shouldParseOneCorectLine() throws Exception {
+    public void shouldParseOneCorrectLine() throws Exception {
         final String line = "30-01-2017,-50,Tesco";
         final BankTransaction result = bankStatementCSVParser.parseFrom(line);
         final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30),
@@ -21,5 +21,41 @@ public class BankStatementCSVParserTest {
         Assert.assertEquals(expected.getDate(), result.getDate());
         Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldHandleEmptyLine() throws Exception {
+        final String line = "";
+        final BankTransaction result = bankStatementCSVParser.parseFrom(line);
+
+        Assert.assertNull(result);
+    }
+    @Test
+    public void shouldHandleInvalidDate() throws Exception {
+        final String line = "30-01-200017,-50,Tesco";
+        final BankTransaction result = bankStatementCSVParser.parseFrom(line);
+
+        Assert.assertNull(result);
+    }
+    @Test
+    public void shouldHandleInvalidDate2() throws Exception {
+        final String line = "s-01-2007,-50,Tesco";
+        final BankTransaction result = bankStatementCSVParser.parseFrom(line);
+
+        Assert.assertNull(result);
+    }
+    @Test
+    public void shouldHandleInvalidDate3() throws Exception {
+        final String line = "80-01-2007,-50,Tesco";
+        final BankTransaction result = bankStatementCSVParser.parseFrom(line);
+
+        Assert.assertNull(result);
+    }
+    @Test
+    public void shouldHandleInvalidDate4() throws Exception {
+        final String line = "v,-50,Tesco";
+        final BankTransaction result = bankStatementCSVParser.parseFrom(line);
+
+        Assert.assertNull(result);
     }
 }
